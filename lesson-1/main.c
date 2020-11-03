2e0af2a16259e8263bcbf92f09573cad9e57daf4
anushkaperera <anushka.uom.tuc@gmail.com>
2020-11-04 00:29:24 +0100

    Add comments to lesson-1/main.c: 	*You can include comments between /* and */ (for both multi or single line comments) or after // for single line comments

--- lesson-1/main.c
+++ lesson-1/main.c
@@ -1,7 +1,15 @@
+/********************************
+ * Author: Anushka Perera
+ * 04 Nov. 2020
+ ********************************/
+
+/********************************
+ * MAIN FUNCTION
+ ********************************/
 #include <stdio.h>
 
 int main()
 {
-	printf("Hello, world\n");
+	printf("Hello, world\n"); // this command prints "Hello, world" to the screen. \n creates newline.
 	return 0;
 }
