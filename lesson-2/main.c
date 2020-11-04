bdfc068c7f6d303759a8c54ea6280e1b4ffd7298
anushkaperera <anushka.uom.tuc@gmail.com>
2020-11-04 01:51:24 +0100

    Edit files folder lesson-2

--- lesson-2/main.c
+++ lesson-2/main.c
@@ -1,6 +1,7 @@
 /*******************************************************************************************************
- * Author: Anushka Perera
- * 04 Nov. 2020
+ * File		: main.c
+ * Author	: Anushka Perera
+ * Date		: 04 Nov. 2020
  *******************************************************************************************************/
 
 /*******************************************************************************************************
@@ -15,6 +16,25 @@
 
 int main()
 {
-	printf("Hello, world\n"); // this command prints "Hello, world" to the screen. \n creates newline.
+	// print sizes of some basic data types
+	printf("size of char          : %ld\n", sizeof(char));
+	printf("size of short int     : %ld\n", sizeof(short));
+	printf("size of int           : %ld\n", sizeof(int));
+	printf("size of long int      : %ld\n", sizeof(long));
+	printf("size of long long int : %ld\n", sizeof(long long));
+	printf("size of float         : %ld\n", sizeof(float));
+	printf("size of double        : %ld\n", sizeof(double));
+	printf("size of long double   : %ld\n", sizeof(long double));
+
+	// variable declarations 
+	char c = 'a'; // define c and initilize
+	short int i, j, k; // define i, j and k without initialization
+	long int long_int = 0x12345678;
+	float f;
+	double df;
+
+	// assign values to previously declared variables
+	f = 12.345e+2;
+	df = 100.001e23;
 	return 0;
 }
